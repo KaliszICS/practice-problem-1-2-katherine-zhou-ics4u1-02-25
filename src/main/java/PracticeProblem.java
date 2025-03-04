cdef0c3323ec7ee1eb7342cc136065e8518433c4
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-03-04 15:39:03 +0000

    Update Practice

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,25 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static boolean validIndex(int[] array, int num) {
+		try{
+			int value = arr[num];
+			return true;
+
+		}
+		catch (ArrayIndexOutOfBoundsException e){
+
+		return false;
+	}
+}
+
+	public static int divide(int a , int b){
+
+	}
+
+	public static int safeConvertStringToInt(String toint){
+
+	}
+
 
 }
